24b31bec681a714c5ef7f3ec76bd084ad6bec522
Akash Srivastava <sri.akash1992@gmail.com>
2023-01-12 16:12:37 +0530

    Add express-rate-limit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit"); // Prevent DDOS
+const cors = require("cors");
 const express = require("express");
 const connectDB = require("./db");
-require("dotenv").config();
 const walletRouter = require("./routes/wallet");
 const transactionRouter = require("./routes/transaction");
 const notFound = require("./middlewares/not-found");
@@ -11,8 +14,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
+// app behind a reverse proxy
+app.set("trust proxy", 1);
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(helmet()); // Add some extra headers for app security
+app.use(cors()); // Allows cross origin resource sharing
 
 app.use("/", walletRouter);
 app.use("/wallet", transactionRouter);
