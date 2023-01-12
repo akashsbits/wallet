47ce07a1caa6fd576af09c6538935ec26ede89d6
Akash Srivastava <sri.akash1992@gmail.com>
2023-01-12 16:48:01 +0530

    Add README.md

--- models/transaction.js
+++ models/transaction.js
@@ -4,7 +4,7 @@ const { v4 } = require("uuid");
 const transactionSchema = new Schema({
   balance: { type: Number, required: true },
   transactionId: { type: String, default: v4 },
-  walletId: { type: String, required: true },
+  walletId: { type: String, index: true, required: true },
   description: { type: String, maxlength: 100, default: null },
   productId: { type: String, default: null },
   type: { type: String, enum: ["credit", "debit"], required: true },
